dbae15f5e3acaa7cc071e3ace192831a628c7ac7
flsouto <fabiolimasouto@gmail.com>
2023-11-20 22:45:02 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
         Not yet :)
-      </div>
     </main>
   )
 }
