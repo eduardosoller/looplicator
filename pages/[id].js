1ec621d8a4348531f997cfe0d05f23f4275f9187
Eduardo Soller <eduardosoller@gmail.com>
2024-01-22 19:47:48 -0300

    del: all

--- pages/[id].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router'
-import data from 'data/123.json'
-import loopURL from 'utils/loop-url'
-
-export default function Page() {
-  const router = useRouter()
-  return <p>Post: {loopURL(data.date, data.id)}</p>
-}
--- utils/loop-url.js
+++
@@ -1,8 +0,0 @@
-import crc32 from './crc32'
-
-const hash = (str) => btoa(crc32(String(str))).substr(2).replace(/=/g,"")
-
-export default (date, seq) => {
-    return "https://s3-aws.etc.com/"+hash(date+"/"+seq)+".mp3";
-}
-
