ad2e8756d7c3df4cf6a5d954b00724a1dcf7ec44
Eduardo Soller <eduardosoller@gmail.com>
2024-05-27 19:32:33 -0300

    fix: remove NEXT_PUBLIC from db vars

--- utils/supabase/admin.ts
+++ utils/supabase/admin.ts
@@ -13,7 +13,7 @@
 // // Note: supabaseAdmin uses the SERVICE_ROLE_KEY which you must only use in a secure server-side context
 // // as it has admin privileges and overwrites RLS policies!
 // const supabaseAdmin = createClient<Database>(
-//   process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+//   process.env.SUPABASE_URL || '',
 //   process.env.SUPABASE_SERVICE_ROLE_KEY || ''
 // );
 
