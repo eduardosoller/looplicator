627c5596973c5b4784d82bb6754ef34e090f9dfc
Eduardo Soller <eduardosoller@gmail.com>
2024-06-06 14:49:42 -0300

    fix: testimonial page - alter testimoinal to comment

--- app/components/Testimonial/index.tsx
+++ app/components/Testimonial/index.tsx
@@ -4,14 +4,14 @@ import styles from "./styles.module.css";
 import QuotesSvg from "../Svg/Quotes";
 import { getRandomTestimonial } from "@/services/testimonial";
 export default async function Testimonial() {
-  const { avatar_url, name, testimonial } = await getRandomTestimonial();
+  const { avatar_url, name, comment } = await getRandomTestimonial();
   return (
     <div className={`col-xl-6 col-lg-12 col-md-12 col-xm-12 ${styles.text}`}>
       <div className={`${styles.card__wrapper}`}>
         <div className={`${styles.card}`}>
           <div className="row">
             <div className={`${styles["col-center"]}`}>
-              <div className={`${styles.title}`}>{testimonial}</div>
+              <div className={`${styles.title}`}>{comment}</div>
               <div className={styles.footer}>
                 {avatar_url && (
                   <Image
